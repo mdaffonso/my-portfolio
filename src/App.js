5b84406f97a79076d78b51926379d8613237ece5
mdaffonso <75502155+mdaffonso@users.noreply.github.com>
2021-03-10 16:25:07 -0300

    version 1.001: fixed 404

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-import { BrowserRouter, Route, Redirect } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import MainStructure from './components/layouts/MainStructure'
 
@@ -30,12 +30,13 @@ function App() {
     <BrowserRouter>
       <GlobalContext.Provider value={{globals, setLanguage, mutateData, toggleLoading, modalContent, setModalContent, modalAnimate, setModal, setActive, activeLink: active, links: globals.sections}}>
         <MainStructure>
-          <Route path='/projects' component={ProjectsScreen} />
-          <Route path='/contact' component={ContactScreen} />
-          <Route path='/about' component={AboutScreen} />
-          <Route path='/404' component={NotFoundScreen} />
-          <Route path='/' component={AboutScreen} exact />
-          <Route render={() => <Redirect to="/404" />} />
+          <Switch>
+            <Route path='/projects' component={ProjectsScreen} />
+            <Route path='/contact' component={ContactScreen} />
+            <Route path='/about' component={AboutScreen} />
+            <Route path='/' component={AboutScreen} exact />
+            <Route component={NotFoundScreen} />
+          </Switch>
         </MainStructure>
       </GlobalContext.Provider>
     </BrowserRouter>
